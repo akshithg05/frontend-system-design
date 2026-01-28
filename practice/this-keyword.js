7fb6bfd74362a5a85be1ba435a8c5c48363e2af8
Akshith G <akshithg01@gmail.com>
2026-01-28 19:05:37 +0530

    Service worker caching

--- practice/this-keyword.js
+++ practice/this-keyword.js
@@ -8,9 +8,36 @@ let obj2 = {
   name: "Anu",
 };
 
-function greet() {
-  console.log(`Hi this is ${this.name}`);
+function greet(city) {
+  console.log(`Hi this is ${this.name}. I come from ${city}`);
 }
 
-greet.call(obj);
-greet.call(obj2);
+greet.call(obj, "Bengaluru");
+greet.call(obj2, "Bangalore");
+
+const greet2 = greet.bind(obj, "Bengaluru");
+
+greet2();
+
+class MyCar {
+  constructor(company, model, variant) {
+    this.company = company;
+    this.model = model;
+    this.variant = variant;
+  }
+
+  getWheels() {
+    return 4;
+  }
+
+  getCompany() {
+    console.log(this.company);
+    return this.company;
+  }
+}
+
+const myCar = new MyCar("Tata", "Safari", "Adventure Persona");
+
+myCar.getCompany();
+
+console.log(this);
