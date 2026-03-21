14f3c724f5ba38df51723d5faa36f3aee1a145ad
Akshith G <akshithg01@gmail.com>
2026-03-21 08:09:21 +0530

    infinite scroll

--- low-level-design/app/src/components/Body.jsx
+++ low-level-design/app/src/components/Body.jsx
@@ -1,44 +1,57 @@
-import { useState, useEffect, useMemo, memo } from "react";
+import { useState, useEffect, useRef, useMemo, memo } from "react";
 import Card from "./Card";
 import ShimmerCard from "./ShimmerCard";
 
-function Body() {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState(null);
+const shimmerArray = Array(20).fill(0);
 
-  const shimmerArray = Array(30).fill(0);
+export default function Body() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const pageRef = useRef(1);
+  const isFetching = useRef(false);
 
   async function fetchData() {
+    if (isFetching.current) return;
+    isFetching.current = true;
     try {
       setLoading(true);
-      const resp = await fetch("https://picsum.photos/v2/list?page=2&limit=30");
-      const data = await resp.json();
-      setData(data);
-      setLoading(false);
+      const resp = await fetch(
+        `https://picsum.photos/v2/list?page=${pageRef.current}&limit=20`,
+      );
+      const json = await resp.json();
+      pageRef.current = pageRef.current + 1;
+      setData((prev) => [...prev, ...json]);
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoading(false);
+      isFetching.current = false;
     }
   }
+
+  function handleScroll() {
+    // scrolly - how much I have scrolled on the web page
+    // innerHeight - visible section
+    // scrollHeight - total height of webpage
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+      fetchData();
+    }
+  }
+
   useEffect(() => {
     fetchData();
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-
-  console.log("Data", data);
 
   const cards = useMemo(() => {
     return data?.map((image) => <Card image={image} key={image.id} />);
   }, [data]);
 
-  if (loading) {
-    return (
-      <div className="px-10 flex flex-wrap gap-4">
-        {shimmerArray?.map((_, i) => {
-          return <ShimmerCard key={i} />;
-        })}
-      </div>
-    );
-  }
-  return <div className="mt-5 px-10 flex flex-wrap gap-4">{cards}</div>;
+  return (
+    <div className="mt-5 px-10 flex flex-wrap gap-4">
+      {cards}
+      {loading && shimmerArray.map((_, i) => <ShimmerCard key={i} />)}
+    </div>
+  );
 }
-
-export default memo(Body);
